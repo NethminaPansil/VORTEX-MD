29cdc86284dce2bc14518013dcc449aac5f707fd
NethminaPansil <nethminapansilu5@gmail.com>
2025-02-02 13:48:17 +0530

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,14 @@
+module.exports = {
+    getMenu: function () {
+        return `📜 *VORTEX MD Menu* 📜\n
+🔹 *.ping* - Check bot status
+🔹 *.info* - Bot Info
+🔹 *.sticker* - Convert image to sticker
+🔹 *.help* - Get help
+🔹 *.owner* - Contact owner
+
+📌 *Use commands with prefix "."*  
+Ex: *.ping*
+        `;
+    }
+};
