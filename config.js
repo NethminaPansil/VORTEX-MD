3e07eac8cf9b7b85527bbe1249799319d87d7fe9
NethminaPansil <nethminapansilu5@gmail.com>
2025-02-09 22:28:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ezgjgYqQ#6ZZ2Nq5hxZN2sWrn9Nzl5heHPNnN1FrG0Rc-64020kw"
+SESSION_ID: process.env.SESSION_ID || "ezgjgYqQ#6ZZ2Nq5hxZN2sWrn9Nzl5heHPNnN1FrG0Rc-64020kw",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/hXXQjMh/b5a6c8f52e0a71c3.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello User,I'm 𝕍𝕆ℝ𝕋𝔼𝕏 𝕄𝔻 Created by Pansilu Nethmina.I'm Alive 😊😊",
 };
